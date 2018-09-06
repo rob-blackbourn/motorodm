cbecb75b22216e08b0ca9988e6a6fbcb86336718
Rob Blackbourn <rblackbourn@ugsb-rbla01.bhdgsystematic.com>
2018-09-06 17:08:18 +0100

    explicitly specify the types

--- motorodm/graphene/converter.py
+++ motorodm/graphene/converter.py
@@ -1,10 +1,7 @@
-from graphene import (ID, Boolean, Dynamic, Field, Float, Int, List,
-                      NonNull, String, is_node)
+import graphene
 from graphene.types.json import JSONString
-
 import motorodm
-
-from motorodm.graphene.fields import MotorOdmConnectionField
+from .fields import MotorOdmConnectionField
 
 from functools import singledispatch
 
@@ -16,31 +13,30 @@ def convert_motorodm_field(field, registry=None):
         (field, field.__class__))
 
 
-@convert_motorodm_field.register(motorodm.fields.StringField)
+@convert_motorodm_field.register(motorodm.StringField)
 def convert_field_to_string(field, registry=None):
-    return String(description=field.db_name, required=field.required)
+    return graphene.String(description=field.db_name, required=field.required)
 
 
-@convert_motorodm_field.register(motorodm.fields.ObjectIdField)
+@convert_motorodm_field.register(motorodm.ObjectIdField)
 def convert_field_to_id(field, registry=None):
-    return ID(description=field.db_name, required=field.required)
+    return graphene.ID(description=field.db_name, required=field.required)
 
 
-@convert_motorodm_field.register(motorodm.fields.IntField)
-# @convert_motorodm_field.register(motorodm.fields.LongField)
+@convert_motorodm_field.register(motorodm.IntField)
 def convert_field_to_int(field, registry=None):
-    return Int(description=field.db_name, required=field.required)
+    return graphene.Int(description=field.db_name, required=field.required)
 
 
-@convert_motorodm_field.register(motorodm.fields.BooleanField)
+@convert_motorodm_field.register(motorodm.BooleanField)
 def convert_field_to_boolean(field, registry=None):
-    return NonNull(Boolean, description=field.db_name)
+    return graphene.NonNull(graphene.Boolean, description=field.db_name)
 
 
-@convert_motorodm_field.register(motorodm.fields.DecimalField)
+@convert_motorodm_field.register(motorodm.DecimalField)
 @convert_motorodm_field.register(motorodm.fields.FloatField)
 def convert_field_to_float(field, registry=None):
-    return Float(description=field.db_name, required=field.required)
+    return graphene.Float(description=field.db_name, required=field.required)
 
 
 # @convert_motorodm_field.register(motorodm.fields.DictField)
@@ -49,37 +45,37 @@ def convert_field_to_float(field, registry=None):
 #     return JSONString(description=field.db_name, required=field.required)
 
 
-@convert_motorodm_field.register(motorodm.fields.DateTimeField)
+@convert_motorodm_field.register(motorodm.DateTimeField)
 def convert_date_to_string(field, registry=None):
-    return String(description=field.db_name, required=field.required)
+    return graphene.String(description=field.db_name, required=field.required)
 
 
-@convert_motorodm_field.register(motorodm.fields.ListField)
+@convert_motorodm_field.register(motorodm.ListField)
 # @convert_motorodm_field.register(motorodm.fields.EmbeddedDocumentListField)
 def convert_field_to_list(field, registry=None):
     # pylint: disable=assignment-from-no-return
     base_type = convert_motorodm_field(field.field, registry=registry)
-    if isinstance(base_type, (Dynamic)):
+    if isinstance(base_type, (graphene.Dynamic)):
         base_type = base_type.get_type()
         if base_type is None:
             return
         base_type = base_type._type
 
-    if is_node(base_type):
+    if graphene.is_node(base_type):
         return MotorOdmConnectionField(base_type)
 
     # Non-relationship field
     relations = (motorodm.fields.ReferenceField,
                  motorodm.fields.EmbeddedDocumentField)
-    if not isinstance(base_type, (List, NonNull)) \
+    if not isinstance(base_type, (graphene.List, graphene.NonNull)) \
             and not isinstance(field.field, relations):
         base_type = type(base_type)
 
-    return List(base_type, description=field.db_name, required=field.required)
+    return graphene.List(base_type, description=field.db_name, required=field.required)
 
 
-@convert_motorodm_field.register(motorodm.fields.EmbeddedDocumentField)
-@convert_motorodm_field.register(motorodm.fields.ReferenceField)
+@convert_motorodm_field.register(motorodm.EmbeddedDocumentField)
+@convert_motorodm_field.register(motorodm.ReferenceField)
 def convert_field_to_dynamic(field, registry=None):
     model = field.document_type
 
@@ -87,6 +83,6 @@ def convert_field_to_dynamic(field, registry=None):
         _type = registry.get_type_for_model(model)
         if not _type:
             return None
-        return Field(_type)
+        return graphene.Field(_type)
 
-    return Dynamic(dynamic_type)
+    return graphene.Dynamic(dynamic_type)
