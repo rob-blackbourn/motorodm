db75f60022e1691741e48a134942fb8a0dd48835
Rob Blackbourn <rblackbourn@ugsb-rbla01.bhdgsystematic.com>
2018-09-07 13:39:59 +0100

    Added more tests

--- tests/test_graphql.py
+++ tests/test_graphql.py
@@ -20,11 +20,13 @@ class TestGraphQL(unittest.TestCase):
             last_name = StringField(db_name='lastName')
 
         async def create(db):
-            await UserModel.qs(db).ensure_indices()
             await UserModel.qs(db).drop()
+            await UserModel.qs(db).ensure_indices()
 
             rob = await UserModel(email='rob.blackbourn@example.com', first_name='Rob', last_name='Blackbourn').qs(db).save()
             ann = await UserModel(email='ann.jones@example.com', first_name='Ann', last_name='Jones').qs(db).save()
+
+            return [rob, ann]
 
         class User(MotorOdmObjectType):
             class Meta:
@@ -46,7 +48,7 @@ class TestGraphQL(unittest.TestCase):
         class Query(graphene.ObjectType):
             users = graphene.List(User)
 
-            async def resolve_users(self, info):
+            async def resolve_users(self, info, email=None):
                 cursor = UserModel.qs(info.context['db']).find()
                 # return await cursor.to_list(100)
                 return [user async for user in cursor]
@@ -55,6 +57,10 @@ class TestGraphQL(unittest.TestCase):
             create_user = CreateUser.Field()
 
         schema = graphene.Schema(query=Query, mutation=Mutation)
+
+        db = self.client.test_motorodm
+        loop = asyncio.get_event_loop()
+        users = loop.run_until_complete(create(db))
 
         query = '''
             query {
@@ -66,6 +72,10 @@ class TestGraphQL(unittest.TestCase):
             }
         '''
 
+        query_result = schema.execute(
+            query, context={'db': db}, executor=AsyncioExecutor(loop=loop))
+        print(query_result)
+
         mutation = '''
             mutation testMutation {
                 createUser(email: "john.doe@example.com", firstName: "John", lastName: "Doe") {
@@ -76,15 +86,23 @@ class TestGraphQL(unittest.TestCase):
             }
         '''
 
-        db = self.client.test_motorodm
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(create(db))
-        query_result = schema.execute(
-            query, context={'db': db}, executor=AsyncioExecutor(loop=loop))
-        print(query_result)
         mutation_result = schema.execute(
             mutation, context={'db': db}, executor=AsyncioExecutor(loop=loop))
         print(mutation_result)
+
+        query2 = '''
+            query {
+                users(email: "rob.blackbourn@example.com") {
+                    id
+                    firstName
+                    lastName
+                }
+            }
+        '''
+
+        query2_result = schema.execute(
+            query2, context={'db': db}, executor=AsyncioExecutor(loop=loop))
+        print(query2_result)
 
 
 if __name__ == '__main__':
