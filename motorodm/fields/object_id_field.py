1bd2b9394093c0d43a444f45757c3da16abe7459
Rob Blackbourn <rblackbourn@ugsb-rbla01.bhdgsystematic.com>
2018-09-07 15:25:10 +0100

    Added mongo transformation for object id

--- motorodm/fields/object_id_field.py
+++ motorodm/fields/object_id_field.py
@@ -1,3 +1,4 @@
+from bson import ObjectId
 from .field import Field
 
 
@@ -5,3 +6,9 @@ class ObjectIdField(Field):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+    def to_mongo(self, value):
+        return ObjectId(value) if value else None
+
+    async def from_mongo(self, value, resolver=None):
+        return str(value) if value else None
