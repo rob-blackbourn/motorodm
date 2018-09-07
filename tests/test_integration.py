1fb40375db34df75651746adb024623247acedf6
Rob Blackbourn <rblackbourn@ugsb-rbla01.bhdgsystematic.com>
2018-09-07 07:49:34 +0100

    Added cleanup to tests

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -21,18 +21,21 @@ class TestIntegration(unittest.TestCase):
             last_name = StringField(db_name='lastName')
 
         db = self.client.test_motorodm
+        await db.drop_collection(User.__collection__)
 
-        await User.qs(db).ensure_indices()
-        await User.qs(db).drop()
+        try:
+            await User.qs(db).ensure_indices()
 
-        user = await User(email='rob.blackbourn@example.com', first_name='Rob', last_name='Blackbourn').qs(db).save()
-        self.assertTrue(user._identity is not None)
+            user = await User(email='rob.blackbourn@example.com', first_name='Rob', last_name='Blackbourn').qs(db).save()
+            self.assertTrue(user._identity is not None)
 
-        users = await User.qs(db).insert_many(
-            User(email='foo@bar.com', first_name='Fred', last_name='Jackson'),
-            User(email='grum@bar.com', first_name='Bob', last_name='Thompson'),
-        )
-        self.assertEqual(len(users), 2)
+            users = await User.qs(db).insert_many(
+                User(email='foo@bar.com', first_name='Fred', last_name='Jackson'),
+                User(email='grum@bar.com', first_name='Bob', last_name='Thompson'),
+            )
+            self.assertEqual(len(users), 2)
+        finally:
+            await db.drop_collection(User.__collection__)
 
     @run_async
     async def test_reference_field(self):
@@ -46,15 +49,18 @@ class TestIntegration(unittest.TestCase):
             body = StringField()
 
         db = self.client.test_motorodm
+        await db.drop_collection(User.__collection__)
+        await db.drop_collection(Post.__collection__)
 
-        await User.qs(db).drop()
-        await Post.qs(db).drop()
+        try:
+            rob = await User(name='Rob').qs(db).save()
+            post = await Post(user=rob, title='My Post', body='Words of wisdom').qs(db).save()
 
-        rob = await User(name='Rob').qs(db).save()
-        post = await Post(user=rob, title='My Post', body='Words of wisdom').qs(db).save()
-
-        post1 = await Post.qs(db).get(post._identity)
-        self.assertEqual(post, post1)
+            post1 = await Post.qs(db).get(post._identity)
+            self.assertEqual(post, post1)
+        finally:
+            await db.drop_collection(User.__collection__)
+            await db.drop_collection(Post.__collection__)
 
     @run_async
     async def test_list_reference_field(self):
@@ -66,16 +72,19 @@ class TestIntegration(unittest.TestCase):
             users = ListField(ReferenceField(User))
 
         db = self.client.test_motorodm
+        await db.drop_collection(User.__collection__)
+        await db.drop_collection(Updaters.__collection__)
 
-        await User.qs(db).drop()
-        await Updaters.qs(db).drop()
+        try:
+            rob = await User(name='Rob').qs(db).save()
+            tom = await User(name='Tom').qs(db).save()
+            updaters = await Updaters(users=[rob, tom]).qs(db).save()
 
-        rob = await User(name='Rob').qs(db).save()
-        tom = await User(name='Tom').qs(db).save()
-        updaters = await Updaters(users=[rob, tom]).qs(db).save()
-
-        updaters1 = await Updaters.qs(db).get(updaters._identity)
-        self.assertEqual(updaters, updaters1)
+            updaters1 = await Updaters.qs(db).get(updaters._identity)
+            self.assertEqual(updaters, updaters1)
+        finally:
+            await db.drop_collection(User.__collection__)
+            await db.drop_collection(Updaters.__collection__)
 
     @run_async
     async def test_find_many(self):
